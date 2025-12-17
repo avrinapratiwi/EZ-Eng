3d3d2237df5528c50049314dd41cfa57e97b4a73
avrinapratiwi <avrinapratiwi12@gmail.com>
2025-12-18 06:10:46 +0700

    mengganti lokasi foto_18

--- resources/views/dashboard-learner.blade.php
+++ resources/views/dashboard-learner.blade.php
@@ -298,15 +298,11 @@
                                                 #{{ $index + 1 }}
                                             @endif
                                         </td>
-                                        <td style="width:50px;">
                                             <td style="width:50px;">
                                                 <img src="{{ $item['learner']->photo ? asset($item['learner']->photo) : asset('images/user.png') }}"
                                                     alt="Learner Photo"
                                                     style="width:40px; height:40px; object-fit:cover; border-radius:50%;">
                                             </td>
-                                        
-                                            
-                                        </td>
                                         <td>{{ $item['learner']->first_name }} {{ $item['learner']->last_name }}</td>
                                         <td>{{ $item['progressPercentage'] }}%</td>
                                     </tr>
