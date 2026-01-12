db8f0bfff15acb4db25b295ad5a150fa9a6cc701
avrinapratiwi <avrinapratiwi12@gmail.com>
2026-01-13 01:17:00 +0700

    berbaiki eror fungsi untuk tombol dwonload sertif 2

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
-use Illuminate\Support\Facades\App;
 use App\Models\Module;
 use App\Models\QuizAttempt;
 use Barryvdh\DomPDF\PDF;
@@ -14,27 +12,17 @@ class ReviewController extends Controller
     public function certificate($moduleId)
     {
         $user = Session::get('user');
-
         $module = Module::findOrFail($moduleId);
 
-        $attempt = QuizAttempt::where('user_id', $user->id)
-            ->latest()
-            ->first();
+        $attempt = QuizAttempt::where('user_id', $user->id)->latest()->first();
 
-        return view('certificate', [
-            'user'   => $user,
-            'module' => $module,
-            'attempt'=> $attempt
-        ]);
+        return view('certificate', compact('user','module','attempt'));
     }
 
-    public function downloadCertificate($moduleId)
+    public function downloadCertificate(PDF $pdf, $moduleId)
     {
         $user = Session::get('user');
         $module = Module::findOrFail($moduleId);
-
-        /** @var PDF $pdf */
-        $pdf = App::make(PDF::class);
 
         $pdf->loadView('certificate', [
             'user'   => $user,
