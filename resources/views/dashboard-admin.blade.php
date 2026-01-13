616c7312af6f08b1c8e5753e112ac6c807ef1a24
avrinapratiwi <avrinapratiwi12@gmail.com>
2026-01-13 09:08:45 +0700

    perbaiki eror total card active learners

--- resources/views/dashboard-admin.blade.php
+++ resources/views/dashboard-admin.blade.php
@@ -205,20 +205,20 @@
                     <!-- Content Row -->
                     <div class="row">
 
-                        <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-3 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card border-left-success shadow h-100 py-2 bg-gradient-success text-white">
                                 <div class="card-body">
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-                                                Total Learners</div>
-                                                <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                                    {{ $totalLearners }}
-                                                </div>
+                                            <div class="text-xs font-weight-bold text-white text-uppercase mb-1">
+                                                Learners Aktif
+                                            </div>
+                                            <div class="h5 mb-0 font-weight-bold text-white">
+                                                {{ $totalActiveLearners }}
+                                            </div>
                                         </div>
                                         <div class="col-auto">
-                                            <i class="fas fa-calendar fa-2x text-gray-300"></i>
+                                            <i class="fas fa-user-check fa-2x text-white-50"></i>
                                         </div>
                                     </div>
                                 </div>
